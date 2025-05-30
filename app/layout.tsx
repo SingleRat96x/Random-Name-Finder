329a2226d5fb412f66729ae3420f452f3df33dc6
Your Name <medshi400@gmail.com>
2025-05-30 15:45:31 +0100

    Test Adsense 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
             async
             src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${adsensePublisherId}`}
             crossOrigin="anonymous"
-            strategy="afterInteractive"
+            
           />
         )}
       </head>
